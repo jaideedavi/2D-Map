715a1351cf1270d9c566c63e1bac969b4a432890
jaideedavi <jaidee.davi@bumail.net>
2026-03-20 18:43:06 +0700

    GreyBox

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
